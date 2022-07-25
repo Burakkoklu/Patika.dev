82478bf15ac3138b3993707ef86ff6886cded314
Burakkoklu <108938767+Burakkoklu@users.noreply.github.com>
2022-07-25 18:00:43 +0300

    Add files via upload

---
+++ sartlarOdev/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        
+        if(N % 2 == 1 || (N<=6 && N <=20 ))
+        {
+            Console.WriteLine("Weird");
+        }
+        else
+        {
+        Console.WriteLine("Not Weird");    
+        }
+         
+    }
+}
