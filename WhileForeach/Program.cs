787ef16b3dec91d778b40023d95572fb783cd2d3
Ömer BurakKöklü <108938767+Burakkoklu@users.noreply.github.com>
2022-07-26 01:23:45 +0300

    Add files via upload

---
+++ WhileForeach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+     static void Main(string[] args)
+    {
+
+        //1 den başlayarak girilen değere kadar olan sayıları toplayıp ortalamasını yazdıran program 
+      Console.WriteLine("Bir Sayı Giriniz:");
+      int sayi=int.Parse(Console.ReadLine());
+      int sayac=1;
+      int toplam=0;
+      while (sayac<=sayi)
+      {
+        toplam+=sayac;
+        sayac++;
+      }
+      Console.WriteLine(toplam/sayi);
+
+       //'a' dan 'z'ye kadar tüm harfleri yazdıran program
+
+       char alfabe='a';
+       while (alfabe<='z')
+       {
+        Console.Write(alfabe);
+        alfabe++;
+       }
+
+       //foreach kullanımı
+       String[] arabalar={"BMW","Ford","Toyota","Nissan"};
+       foreach (var araba in arabalar)
+       {
+        Console.WriteLine(araba);
+        
+       }
+
+        }
+    }
