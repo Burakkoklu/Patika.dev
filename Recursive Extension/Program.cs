5c71761b03e432cfff4ae89335180fa34629fde5
Ömer BurakKöklü <108938767+Burakkoklu@users.noreply.github.com>
2022-07-27 01:13:06 +0300

    Add files via upload

---
+++ Recursive Extension/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+     static void Main(string[] args)
+    {
+        Islemler customer=new();
+        Console.WriteLine(customer.Expo(3,4));
+        
+       }
+       public class Islemler{
+        public int Expo(int sayi, int üs){
+            if(üs<2)
+            return sayi;
+            return Expo(sayi,üs-1)*sayi;
+        }
+       }
+     
+ 
+ }
+       
